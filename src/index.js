623d7468e8333a395d0382ba9999f037e72946f5
kimtj123 <kimtajung1@gmail.com>
2019-10-30 20:44:36 +0900

    antDesign 디펜던시 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// env에 관해서 다음 주소를 참조 https://velopert.com/3417
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
