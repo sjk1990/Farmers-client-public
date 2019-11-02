b9aa4acd3ef1a60c6787440e820f2677e5bd7a06
김승진 <worldsjk1990@gmail.com>
2019-11-02 15:30:08 +0900

    라우팅 디버깅 완료

--- src/mainPage/component/header.js
+++ src/mainPage/component/header.js
@@ -1,48 +1,46 @@
-import React from 'react';
-import { Button, PageHeader } from 'antd';
-// import Header from './mainPage/component/header';
-// import Body from './mainPage/component/body';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import WrappedSignIn from '../../signPage/signIn';
-import WrappedSignUp from '../../signPage/signUp';
-import Homepage from '../../signPage/router-home';
+// import React from 'react';
+// import { Button, PageHeader } from 'antd';
+// // import Header from './mainPage/component/header';
+// // import Body from './mainPage/component/body';
+// import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+// import WrappedSignIn from '../../signPage/signIn';
+// import WrappedSignUp from '../../signPage/signUp';
 
-/***********************************************CSS*************************************************************/
+// /***********************************************CSS*************************************************************/
 
-const PageHeaderStyle = { width: '70vw', margin: 'auto', marginTop: '1vh', borderRadius: '1.5vh'}
+// const PageHeaderStyle = { width: '70vw', margin: 'auto', marginTop: '1vh', borderRadius: '1.5vh' }
 
-/***************************************************************************************************************/
+// /***************************************************************************************************************/
 
-function Topper() {
-  return (
-  <Router>
-    <div className="header-wrap">
-      <PageHeader
-        style={PageHeaderStyle}
-        ghost={false}
-        title="생활농사"
-        // onBack={() => window.history.back()}
-        // subTitle="This is a subtitle"
-        extra={[
-          <Button key="3">
-              <Link to="/homepage">HOME</Link>
-            </Button>,
-            <Button key="2">
-              <Link to="/signUp">회원가입</Link>
-            </Button>,
-            <Button key="1" type="primary">
-              <Link to="/signIn">로그인</Link>
-            </Button>
-        ]}
-      ></PageHeader>
-        <Switch>
-          <Route path="/signIn" component={WrappedSignIn} />
-          <Route path="/signUp" component={WrappedSignUp} />
-          <Route path="/homepage" component={Homepage} />
-        </Switch>
-    </div>
-  </Router>
-    );
-  }
+// function Topper() {
+//   return (
+//     <Router>
+//       <div className="header-wrap">
+//         <PageHeader
+//           style={PageHeaderStyle}
+//           ghost={false}
+//           title="생활농사"
+//           // onBack={() => window.history.back()}
+//           // subTitle="This is a subtitle"
+//           extra={[
+//             <Button key="3">
+//               <Link to="/homepage">HOME</Link>
+//             </Button>,
+//             <Button key="2">
+//               <Link to="/signUp">회원가입</Link>
+//             </Button>,
+//             <Button key="1" type="primary">
+//               <Link to="/signIn">로그인</Link>
+//             </Button>
+//           ]}
+//         ></PageHeader>
+//         <Switch>
+//           <Route path="/signIn" component={WrappedSignIn} />
+//           <Route path="/signUp" component={WrappedSignUp} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
 
-export default Topper;
+// export default Topper;
