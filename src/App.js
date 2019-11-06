239fa26efdedece4200dfd88f5f8c642198166a5
김승진 <worldsjk1990@gmail.com>
2019-11-06 10:34:49 +0900

    화면전환 완성?

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Layout } from "antd";
 import "./App.css";
 import Body from "./mainPage/component/body";
 import Bottom from "./mainPage/component/footer";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 import { Button, PageHeader } from "antd";
 import WrappedSignIn from "./signPage/signIn";
 import WrappedSignUp from "./signPage/signUp";
 import { CoverageSummary } from "istanbul-lib-coverage";
 import { sign } from "crypto";
+import { animationFrameScheduler } from "rxjs";
 const PageHeaderStyle = {
   width: "70vw",
   margin: "auto",
@@ -21,7 +22,8 @@ class App extends Component {
     super(props);
     this.state = {
       signIn: false,
-      signUp: false
+      signUp: false,
+      // isLogin: false
     };
 
     this.infoRegister = this.infoRegister.bind(this);
@@ -84,14 +86,44 @@ class App extends Component {
       .then(res => {
         if (res.status === 200) {
           this.setState({ signIn: true });
-          return console.log('로그인되었습니다.');
+          return alert('로그인되었습니다.');
         }
       })
       // .then(res => this.setState({ signIn: false }))
       .catch(err => console.error(err));
   }
 
+  //로그인 되면 바로 렌더링 될 수 있게
+  //로그인 시 true인 state값(signIn: true)에 따라 실행되도록?
+  getCrops() {
+    let url = "http://localhost:5000/crop/reco";
+    // let url = "http://e218b6bc.ngrok.io/crop/reco";
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      if (this.state.signIn && (res.status === 200)) { // 로그인 조건 만족?
+        return res.json();
+      }
+    })
+      // .then( // 렌더링 방식 결정
+      //   callback? body-recommend-ptag 안에 렌더링 해 준다.
+      //   body-recommend도 signIn 의 state에 따라 렌더링 되도록.
+      //   서버에서 배열속에 정보를 담아 리턴해줌)
+      .catch(err => console.error(err));
+  }
+
   render() {
+    let toggleLogin = !this.state.signIn ?
+      <Button key="1" type="primary"><Link to="/signIn">로그인</Link></Button> :
+      // this.state.signIn ?
+      <Button onClick={() => this.setState({ signIn: false })} key="1" type="danger"><Link to="/signIn">로그아웃</Link></Button>
+    // : <Button key="1" type="primary"><Link to="/signIn">로그인</Link></Button>
+    // if (this.state.isLogin === true) {
+    //   return (<Redirect to="/signIn" from="/"></Redirect>)
+    // }
     return (
       <Router>
         <Layout className="layout">
@@ -104,14 +136,15 @@ class App extends Component {
                 <Button key="2">
                   <Link to="/signUp">회원가입</Link>
                 </Button>,
-                <Button key="1" type="primary">
-                  <Link to="/signIn">로그인</Link>
-                </Button>
+                toggleLogin
+                // <Button key="1" type="primary">
+                //   <Link to="/signIn">로그인</Link>
+                // </Button>
               ]}
             ></PageHeader>
             <Switch>
-              {/* <Route exact path="/" component={() => <Body />} /> */}
-              <Route exact path="/" component={Body} />
+              {/* <Route exact path="/" component={Body} /> */}
+              <Route exact path="/" render={() => <Body />} />
               <Route path="/signIn" render={() => <WrappedSignIn func={this.tryLogin} signInBool={this.state.signIn} />} />
               <Route path="/signUp" render={() => <WrappedSignUp func={this.infoRegister} signUpBool={this.state.signUp} />} />
             </Switch>
